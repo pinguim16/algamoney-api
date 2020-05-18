07a2a49e0b14b734071d4335d2b84c28a355fc5d
César Augusto <cesaraugustosi16@gmail.com>
2020-05-18 19:48:49 -0300

    Add token no cookie

--- src/main/java/com/algamoney/repository/CategoriaRepository.java
+++ src/main/java/com/algamoney/repository/CategoriaRepository.java
@@ -1,9 +1,8 @@
 package com.algamoney.repository;
 
+import com.algamoney.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.algamoney.model.Categoria;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
--- src/main/java/com/algamoney/resource/PessoaResource.java
+++ src/main/java/com/algamoney/resource/PessoaResource.java
@@ -1,22 +1,17 @@
 package com.algamoney.resource;
 
 import com.algamoney.event.RecursoCriadoEvent;
-import com.algamoney.model.Categoria;
 import com.algamoney.model.Pessoa;
 import com.algamoney.repository.PessoaRepository;
 import com.algamoney.service.PessoaService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
