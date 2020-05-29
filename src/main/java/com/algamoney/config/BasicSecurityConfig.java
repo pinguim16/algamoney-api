7658892d19ca3a56a91dd8a82bcd4d7d6b7cdaa9
César Augusto <cesaraugustosi16@gmail.com>
2020-05-29 19:59:51 -0300

    Validaçoes lancamento

--- src/main/java/com/algamoney/config/BasicSecurityConfig.java
+++ src/main/java/com/algamoney/config/BasicSecurityConfig.java
@@ -12,9 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@EnableWebSecurity
-//@Profile("basic-security")
-//@Order(99)
+@EnableWebSecurity
+@Profile("basic-security")
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
