22a77c94783c39f5b46133be976b0a249431359b
César Augusto <cesaraugustosi16@gmail.com>
2020-05-29 21:07:54 -0300

    Alterações

--- src/main/java/com/algamoney/config/AuthorizationServerConfig.java
+++ src/main/java/com/algamoney/config/AuthorizationServerConfig.java
@@ -34,7 +34,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                     .withClient("angular")
-                    .secret("$2a$10$G1j5Rf8aEEiGc/AET9BA..xRR.qCpOUzBZoJd8ygbGy6tb3jsMT9G")
+                    .secret("@ngul@r0")
                     .scopes("read", "write")
                     .authorizedGrantTypes("password","refresh_token")
                     .accessTokenValiditySeconds(1800)
